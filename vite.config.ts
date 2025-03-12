e64b1f16ff724ee526bc36232dc76be01d6b711f
Igor Alencar <igorvcalencar@gmail.com>
2025-03-12 14:45:29 -0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
-})
+  build: {
+    // ajuste do limite de tamanho de chunk para evitar aviso
+    chunkSizeWarningLimit: 1000, // aumentar o limite para 1000 KB, ou conforme necessário
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
